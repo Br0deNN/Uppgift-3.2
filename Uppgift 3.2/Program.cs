73b24ec25a55b497cb998eb9d3a531a59ac5d1a8
7R1XY <theo.broden@gmail.com>
2023-10-04 10:39:58 +0200

    Add project files.

---
+++ Uppgift 3.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Uppgift_3._2
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Har du gått ut gymnasiet? Svara J för ja och N för nej");
+            string gymnasie = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            string siffra = Console.ReadLine();
+            int ålder = int.Parse(siffra);
+
+            if (ålder < 22 && gymnasie == "j" )
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvär efter andra");
+            }
+        
+
+         
+            Console.ReadKey();
+
+            
+        }
+    }
+}
